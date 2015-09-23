103f45c501ef5d1a3b5af287bd894887d270145a
Paul <linp21847@gmail.com>
2015-09-23 14:49:33 +0200

    values stayed in popup

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -6,24 +6,29 @@
 	}, function(response) {
 		val1 = response.input1,
 		val2 = response.input2;
-	});
 
-	$(document).ready(function() {
-		$("#value1").val(val1);
-		$("#value2").val(val2);
-		$("#submit").click(function() {
-			var input1 = $("#value1").val(),
-				input2 = $("#value2").val();
+		$(document).ready(function() {
+			$("#value1").val(val1).change(function(e) {
+				localStorage.setItem("input1", JSON.parse($(this).val()));
+			});
+			$("#value2").val(val2).change(function(e) {
+				localStorage.setItem("input2", JSON.parse($(this).val()));
+			});
+			$("#submit").click(function() {
+				var input1 = $("#value1").val(),
+					input2 = $("#value2").val();
 
-			chrome.extension.sendMessage({
-				msg: "from-popup",
-				data: {
-					input1: input1,
-					input2: input2
-				}
-			}, function(response) {
-				console.log(response);
+				chrome.extension.sendMessage({
+					msg: "from-popup",
+					data: {
+						input1: input1,
+						input2: input2
+					}
+				}, function(response) {
+					console.log(response);
+				});
 			});
 		});
+
 	});
 })(window, $);
