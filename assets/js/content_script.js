6d8b0376c77107728a8ffd0e5cdac0c29df39a94
Paul <linp21847@gmail.com>
2015-09-28 14:24:41 +0200

    drag and drop example was added

--- assets/js/content_script.js
+++ assets/js/content_script.js
@@ -64,6 +64,6 @@
 					fillInMSKU(response);
 				});
 			}
-		})
+		});
 	});
 })(window, $)
--- assets/js/options.js
+++ assets/js/options.js
@@ -1,17 +1,4 @@
+	$( "#sortable" ).disableSelection();
+	$( "#sortable" ).sortable();
 $(document).ready(function() {
-	// $("#hotkey").keyup(function(e) {
-	// 	var keys = [],
-	// 		curKey = String.fromCharCode(e.which);
-
-	// 	if (e.ctrlKey)
-	// 		keys.push("Ctrl");
-	// 	else if (e.shiftKey)
-	// 		keys.push("Shift");
-
-	// 	if ( curKey && curKey !== "." ) {
-	// 		keys.push(curKey);
-
-	// 		$(this).val(keys.join(" + "));
-	// 	}
-	// })
 })
